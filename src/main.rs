2492b10e67581e32858e552071c6f832b4b8a4a4
Andrew Morris <voltrevo@gmail.com>
2024-09-18 09:16:42 +1000

    cache evaluations and slowly change data set

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
+use std::collections::HashMap;
+
 use board_eval::BoardEval;
 use game::Game;
 use nelder_mead::NelderMead;
-use rand::{thread_rng, Rng};
 
 mod board;
 mod board_eval;
@@ -11,47 +12,39 @@ mod nelder_mead;
 mod piece;
 mod piece_type_generator;
 
+type Cache = HashMap<usize, HashMap<u64, usize>>;
+
 fn main() {
-    // first_explore();
+    first_explore();
 
-    let res = vec![
-        //     -1.3629023,    // line value
-        //     1.0010495,     // x1
-        //     2.5488162,     // overhangs
-        //     0.7336444,     // holes
-        //     -2.3325214,    // readiness depth
-        //     1.1052921,     // tetris ready
-        //     -0.0116937645, // good patterns
-        //     0.26175177,    // bad patterns
-        //     0.8194094,     // very bad patterns
-        //     2.1194324,     // height
-        0.11743796,  // line value
-        -1.2541437,  // x1
-        -1.3426046,  // overhangs
-        0.06455171,  // holes
-        1.0662355,   // readiness depth
-        0.11092834,  // tetris ready
-        0.013263656, // good patterns
-        -0.27358586, // bad patterns
-        -0.35926318, // very bad patterns
-        -1.3546457,  // height
-    ];
+    // let res = vec![
+    //     0.11743796,  // line value
+    //     -1.2541437,  // x1
+    //     -1.3426046,  // overhangs
+    //     0.06455171,  // holes
+    //     1.0662355,   // readiness depth
+    //     0.11092834,  // tetris ready
+    //     0.013263656, // good patterns
+    //     -0.27358586, // bad patterns
+    //     -0.35926318, // very bad patterns
+    //     -1.3546457,  // height
+    // ];
 
-    println!("eval {}", eval_point(&res, &mut (), 0));
+    // println!("eval {}", eval_point(&res, &mut Cache::default(), 0));
 
-    let mut game = Game::new(2, BoardEval(res.clone()));
+    // let mut game = Game::new(2, BoardEval(res.clone()));
 
-    println!("{:?}", &game);
+    // println!("{:?}", &game);
 
-    let mut i = 0;
+    // let mut i = 0;
 
-    while !game.board.finished {
-        game.step();
-        i += 1;
-        println!("step {}: {:?}", i, &game);
-    }
+    // while !game.board.finished {
+    //     game.step();
+    //     i += 1;
+    //     println!("step {}: {:?}", i, &game);
+    // }
 
-    println!("{:?}", &res);
+    // println!("{:?}", &res);
 }
 
 #[allow(dead_code)]
@@ -60,14 +53,11 @@ fn first_explore() {
         .map(|i| BoardEval::rand(i as u64).0)
         .collect::<Vec<_>>();
 
-    let mut nm = NelderMead::<()>::new(init_simplex);
+    let mut nm = NelderMead::<Cache>::new(init_simplex);
 
     let res = nm.optimize(eval_point, 1e-4, 50);
 
-    let res_score = eval_point(&res, &mut (), 0);
-
-    println!("{:?}", &res);
-    println!("{}", res_score);
+    let res_score = eval_point(&res, &mut Cache::default(), 0);
 
     let mut game = Game::new(2, BoardEval(res.clone()));
 
@@ -82,17 +72,39 @@ fn first_explore() {
     }
 
     println!("{:?}", &res);
+    println!("{}", res_score);
 }
 
-fn eval_point(point: &Vec<f32>, _state: &mut (), _iter: usize) -> f32 {
+fn hash_point(point: &Vec<f32>) -> u64 {
+    // this is a little hacky.. it's easy to construct collisions
+    // however, it shouldn't be a problem in our use case
+    point.into_iter().map(|x| x.to_bits() as u64).sum()
+}
+
+fn eval_point(point: &Vec<f32>, cache: &mut Cache, iter: usize) -> f32 {
     let board_eval = BoardEval(point.clone());
 
     let iters = 20;
-    let mut score_sum = 0.0;
+    let mut score_sum = 0;
+    let start = 1 + iter / 5;
 
-    for _ in 0..iters {
-        let mut rng = thread_rng();
-        let mut game = Game::new(rng.gen(), board_eval.clone());
+    cache.remove(&(start - 1));
+
+    for seed in start..(start + iters) {
+        let hpoint = hash_point(point);
+
+        let cached_score = cache
+            .get(&seed)
+            .map(|subcache| subcache.get(&hpoint))
+            .flatten()
+            .cloned();
+
+        if let Some(cached_score) = cached_score {
+            score_sum += cached_score;
+            continue;
+        }
+
+        let mut game = Game::new(seed as u64, board_eval.clone());
 
         while !game.board.finished {
             game.step();
@@ -100,10 +112,15 @@ fn eval_point(point: &Vec<f32>, _state: &mut (), _iter: usize) -> f32 {
 
         // dbg!(game.board.score);
 
-        score_sum += game.board.score as f32;
+        cache
+            .entry(seed)
+            .or_default()
+            .insert(hash_point(point), game.board.score);
+
+        score_sum += game.board.score;
     }
 
-    let res = -score_sum / (iters as f32);
+    let res = -(score_sum as f32) / (iters as f32);
 
     dbg!(res);
 
