b5d37da6c14ef0488d3058892c90935267c0db40
Andrew Morris <voltrevo@gmail.com>
2024-09-19 09:18:52 +1000

    wip

---
+++ src/PieceType.ts
@@ -0,0 +1,127 @@
+enum PieceType {
+    I,
+    O,
+    J,
+    L,
+    S,
+    Z,
+    T,
+}
+
+enum RotateDir {
+    Cw,
+    Ccw,
+}
+
+const PIECE_GRIDS: { [key in PieceType]: number[] } = {
+    [PieceType.I]: [
+        0b0000_0000_1111_0000,
+        0b0010_0010_0010_0010,
+        0b0000_0000_1111_0000,
+        0b0010_0010_0010_0010,
+    ],
+    [PieceType.O]: [
+        0b0000_0110_0110_0000,
+        0b0000_0110_0110_0000,
+        0b0000_0110_0110_0000,
+        0b0000_0110_0110_0000,
+    ],
+    [PieceType.J]: [
+        0b0000_1110_0010_0000,
+        0b0100_0100_1100_0000,
+        0b1000_1110_0000_0000,
+        0b0110_0100_0100_0000,
+    ],
+    [PieceType.L]: [
+        0b0000_1110_1000_0000,
+        0b1100_0100_0100_0000,
+        0b0010_1110_0000_0000,
+        0b0100_0100_0110_0000,
+    ],
+    [PieceType.S]: [
+        0b0000_0110_1100_0000,
+        0b0100_0110_0010_0000,
+        0b0000_0110_1100_0000,
+        0b0100_0110_0010_0000,
+    ],
+    [PieceType.Z]: [
+        0b0000_1100_0110_0000,
+        0b0010_0110_0100_0000,
+        0b0000_1100_0110_0000,
+        0b0010_0110_0100_0000,
+    ],
+    [PieceType.T]: [
+        0b0000_1110_0100_0000,
+        0b0100_1100_0100_0000,
+        0b0100_1110_0000_0000,
+        0b0100_0110_0100_0000,
+    ],
+};
+
+function grids(pieceType: PieceType): number[] {
+    return PIECE_GRIDS[pieceType];
+}
+
+function findGridIndex(grids: number[], grid: number): number {
+    for (let i = 0; i < grids.length; i++) {
+        if (grids[i] === grid) {
+            return i;
+        }
+    }
+    throw new Error("Grid not found");
+}
+
+class Piece {
+    type_: PieceType;
+    grid: number;
+    pos: [number, number];
+
+    constructor(type_: PieceType, grid: number, pos: [number, number]) {
+        this.type_ = type_;
+        this.grid = grid;
+        this.pos = pos;
+    }
+
+    numRotations(): number {
+        switch (this.type_) {
+            case PieceType.I:
+            case PieceType.S:
+            case PieceType.Z:
+                return 2;
+            case PieceType.O:
+                return 1;
+            case PieceType.J:
+            case PieceType.L:
+            case PieceType.T:
+                return 4;
+        }
+    }
+
+    rotate(dir: RotateDir): void {
+        const offset = dir === RotateDir.Cw ? 1 : 3;
+        const gridsForType = grids(this.type_);
+        const gridIndex = findGridIndex(gridsForType, this.grid);
+        this.grid = gridsForType[(gridIndex + offset) % 4];
+    }
+
+    at(i: number, j: number): boolean {
+        return (this.grid & (1 << (4 * i + j))) !== 0;
+    }
+
+    cellPositions(): [number, number][] {
+        const res: [number, number][] = [];
+        for (let i = 0; i < 16; i++) {
+            const mask = 1 << (15 - i);
+            if ((this.grid & mask) !== 0) {
+                res.push([
+                    this.pos[0] + Math.floor(i / 4),
+                    this.pos[1] + (i % 4),
+                ]);
+                if (res.length === 4) {
+                    return res;
+                }
+            }
+        }
+        throw new Error("Failed to find 4 cells in tetromino");
+    }
+}
