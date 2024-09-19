980fa0f7d837c865a1802c3de1137b56a95ea36f
Andrew Morris <voltrevo@gmail.com>
2024-09-20 08:21:01 +1000

    Add evaluateModel

---
+++ programs/evaluateModel.ts
@@ -0,0 +1,34 @@
+import { Board } from "../src/Board";
+import { generateGameBoards } from "../src/generateGameBoards";
+import { createBoardEvaluator } from "../src/model";
+import { WelfordCalculator } from "../src/WelfordCalculator";
+import { loadModel } from "./helpers/modelStorage";
+
+async function evaluateModel() {
+    console.log('loading model');
+    let model = await loadModel();
+
+    const calc = new WelfordCalculator();
+
+    // Create a board evaluator using the blank model
+    let boardEvaluator = createBoardEvaluator(model);
+
+    while (true) {
+        for (let i = 0; i < 100; i++) {
+            const { finalScore } = generateGameBoards(new Board(10), boardEvaluator);
+            calc.update(finalScore);
+        }
+
+        const mean = calc.getMean();
+        const stdev = calc.getStdev();
+        const metaStdev = stdev / Math.sqrt(calc.n);
+
+        // This is a little crude because it assumes stdev is the true value, but it seems to work
+        // more than well enough for our purposes
+        const rel2StdevError = 2 * metaStdev / mean;
+
+        console.log(`${Math.round(mean)} ± ${(100 * rel2StdevError).toFixed(1)}% (n=${calc.n})`);
+    }
+}
+
+evaluateModel().catch(console.error);
---
+++ src/WelfordCalculator.ts
@@ -0,0 +1,34 @@
+export class WelfordCalculator {
+    public n: number; // Count of data points
+    private mean: number; // Mean of the data points
+    private M2: number; // Sum of squared differences from the mean (used for variance)
+
+    constructor() {
+        this.n = 0;
+        this.mean = 0;
+        this.M2 = 0;
+    }
+
+    // Method to add a new data point
+    public update(x: number): void {
+        this.n += 1;
+        const delta = x - this.mean;
+        this.mean += delta / this.n;
+        const delta2 = x - this.mean; // delta after updating mean
+        this.M2 += delta * delta2;
+    }
+
+    // Method to get the mean of the data points
+    public getMean(): number {
+        return this.mean;
+    }
+
+    // Method to get the standard deviation of the data points
+    public getStdev(): number {
+        if (this.n < 2) {
+            return NaN; // Standard deviation is undefined for fewer than two values
+        }
+        const variance = this.M2 / (this.n - 1); // Sample variance
+        return Math.sqrt(variance);
+    }
+}
