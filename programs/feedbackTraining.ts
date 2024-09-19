87714eb5011996dd35714e6121e083ccf279f828
Andrew Morris <voltrevo@gmail.com>
2024-09-19 17:33:42 +1000

    Use saved training data in feedback training

--- programs/feedbackTraining.ts
+++ programs/feedbackTraining.ts
@@ -2,13 +2,15 @@ import { generateTrainingData, TrainingDataPair } from "../src/generateTrainingD
 import { createBoardEvaluator } from "../src/model";
 import { showPerformanceSummary } from "../src/showPerformanceSummary";
 import { trainModel } from "../src/train";
-import { loadModel, saveModel } from "./helpers/modelStorage";
+import { loadModel, loadTrainingData, saveModel, saveTrainingData } from "./helpers/modelStorage";
 
 async function feedbackTraining() {
+    const startTime = Date.now();
+
     console.log('loading model');
     let model = await loadModel();
 
-    let trainingData: TrainingDataPair[] = [];
+    let trainingData: TrainingDataPair[] = (await loadTrainingData()) ?? [];
 
     while (true) {
         // Create a board evaluator using the blank model
@@ -20,7 +22,10 @@ async function feedbackTraining() {
 
         while (trainingData.length < 5000) {
             trainingData.push(...generateTrainingData(boardEvaluator, 1000));
+            console.log('trainingData.length', trainingData.length);
         }
+
+        await saveTrainingData(trainingData);
 
         // Train the model on the training data
         model = await trainModel(model, trainingData, 50);
@@ -28,7 +33,8 @@ async function feedbackTraining() {
         // Use the updated model to replace the training data
         boardEvaluator = createBoardEvaluator(model);
 
-        showPerformanceSummary(boardEvaluator, trainingData);
+        showPerformanceSummary(boardEvaluator);
+        console.log(`Training for ${((Date.now() - startTime) / 60_000).toFixed(1)} minutes`);
 
         await saveModel(model);
     }
