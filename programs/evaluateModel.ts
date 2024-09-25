bf6e2ebac904f5bc5e5b61c1f4ebc277a5a54ccc
Andrew Morris <voltrevo@gmail.com>
2024-09-25 12:46:00 +1000

    Mixed results from dropout

--- programs/evaluateModel.ts
+++ programs/evaluateModel.ts
@@ -2,18 +2,18 @@ import { Board } from "../src/Board";
 import { generateGameBoards } from "../src/generateGameBoards";
 import { WelfordCalculator } from "../src/WelfordCalculator";
 import { stdMaxLines } from "../src/hyperParams";
-import { ScoreModel } from "../src/ScoreModel";
+import { PredictionModel } from "../src/PredictionModel";
 
 async function evaluateModel() {
     console.log('loading model');
-    let model = await ScoreModel.load();
+    let model = await PredictionModel.load();
 
     const calc = new WelfordCalculator();
 
     let boardEvaluator = model.createBoardEvaluator();
 
     while (true) {
-        for (let i = 0; i < 100; i++) {
+        for (let i = 0; i < 10; i++) {
             const { finalScore } = generateGameBoards(new Board(stdMaxLines), boardEvaluator);
             calc.update(finalScore);
         }
